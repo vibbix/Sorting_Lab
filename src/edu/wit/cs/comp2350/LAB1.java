6cbedf46ecf321c84b536a9f7c3574efa420e33c
end user <kreimendahlf@wit.edu>
2017-01-10 04:49:42 -0500

    added lab1 project

---
+++ src/edu/wit/cs/comp2350/LAB1.java
@@ -0,0 +1,119 @@
+package edu.wit.cs.comp2350;
+
+/* Sorts integers from command line using various algorithms 
+ * 
+ * Wentworth Institute of Technology
+ * COMP 2350
+ * Programming Assignment 0
+ * 
+ */
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class LAB1 {
+
+	public final static int MAX_INPUT = 524287;
+	public final static int MIN_INPUT = 0;
+
+	// TODO: document this method
+	public static int[] countingSort(int[] a) {
+		//TODO: implement this method
+		return null;
+	}
+
+	// TODO: document this method
+	public static int[] radixSort(int[] a) {
+		// TODO: implement this method
+		return null;
+	}
+
+	/********************************************
+	 * 
+	 * You shouldn't modify anything past here
+	 * 
+	********************************************/
+	
+	// example sorting algorithm
+	public static int[] insertionSort(int[] a) {
+
+		for (int i = 1; i < a.length; i++) {
+			int tmp = a[i];
+			int j;
+			for (j = i-1; j >= 0 && tmp < a[j]; j--)
+				a[j+1] = a[j];
+			a[j+1] = tmp;
+		}
+		
+		return a;
+	}
+
+	/* Implementation note: The sorting algorithm is a Dual-Pivot Quicksort by Vladimir Yaroslavskiy,
+	 *  Jon Bentley, and Joshua Bloch. This algorithm offers O(n log(n)) performance on many data 
+	 *  sets that cause other quicksorts to degrade to quadratic performance, and is typically 
+	 *  faster than traditional (one-pivot) Quicksort implementations. */
+	public static int[] systemSort(int[] a) {
+		Arrays.sort(a);
+		return a;
+	}
+
+	// read ints from a Scanner
+	// returns an array of the ints read
+	private static int[] getInts(Scanner s) {
+		ArrayList<Integer> a = new ArrayList<Integer>();
+
+		while (s.hasNextInt()) {
+			int i = s.nextInt();
+			if ((i <= MAX_INPUT) && (i >= MIN_INPUT))
+				a.add(i);
+		}
+
+		return toIntArray(a);
+	}
+
+	// copies an ArrayList to an array
+	private static int[] toIntArray(ArrayList<Integer> a) {
+		int[] ret = new int[a.size()];
+		for(int i = 0; i < ret.length; i++)
+			ret[i] = a.get(i);
+		return ret;
+	}
+
+	
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		
+		System.out.printf("Enter the sorting algorithm to use ([c]ounting, [r]adix, [i]nsertion, or [s]ystem): ");
+		char algo = s.next().charAt(0);
+		
+		System.out.printf("Enter the integers that you would like sorted: ");
+		int[] unsorted_values = getInts(s);
+		int[] sorted_values = {};
+
+		s.close();
+
+		switch (algo) {
+		case 'c':
+			sorted_values = countingSort(unsorted_values);
+			break;
+		case 'r':
+			sorted_values = radixSort(unsorted_values);
+			break;
+		case 'i':
+			sorted_values = insertionSort(unsorted_values);
+			break;
+		case 's':
+			sorted_values = systemSort(unsorted_values);
+			break;
+		default:
+			System.out.println("Invalid sorting algorithm");
+			System.exit(0);
+			break;
+		}
+		
+		System.out.println(Arrays.toString(sorted_values));
+		
+	}
+
+}
